f80c3dbd7b1058ebdc38c8717f37ee596b47cdc0
abemassry <abe@massindustries.org>
2012-07-23 20:57:19 -0400

    fixed controller typo

--- application/controllers/helloworld.php
+++ application/controllers/helloworld.php
@@ -4,7 +4,7 @@
         {  
             $this->load->model('helloworld_model');  
   
-            $data['result'] = $this->helloworld_model-><span class="sql">getData</span>();  
+            $data['result'] = $this->helloworld_model->'<span class="sql">getData</span>'();  
             $data['page_title'] = "CI Hello World App!";  
   
             $this->load->view('helloworld_view',$data);  
