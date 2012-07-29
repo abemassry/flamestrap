ac98939db57ead3bb69551736c1f19e55ea0b2dd
abemassry <abe@massindustries.org>
2012-07-29 17:37:02 -0400

    added news views and changed routes, i might change routes back to the original if this doesnt work

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,8 +41,13 @@
 //$route['default_controller'] = 'pages/view';
 //$route['(:any)'] = 'pages/view/$1';
 
-$route['default_controller'] = "Helloworld";
-$route['404_override'] = '';
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
+$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
+
+// $route['default_controller'] = "Helloworld";
+// $route['404_override'] = '';
 
 
 /* End of file routes.php */
