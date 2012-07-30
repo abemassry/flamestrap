15fb29de25c6979372e899abc00072a18165a519
abemassry <abe@massindustries.org>
2012-07-30 17:01:46 -0400

    was this the first route?

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,16 +38,16 @@
 |
 */
 
-$route['default_controller'] = 'pages/view';
-$route['(:any)'] = 'pages/view/$1';
+// $route['default_controller'] = 'pages/view';
+// $route['(:any)'] = 'pages/view/$1';
 
 // $route['news/(:any)'] = 'news/view/$1';
 // $route['news'] = 'news';
 // $route['(:any)'] = 'pages/view/$1';
 // $route['default_controller'] = 'pages/view';
 
-// $route['default_controller'] = "Helloworld";
-// $route['404_override'] = '';
+$route['default_controller'] = "Helloworld";
+$route['404_override'] = '';
 
 
 /* End of file routes.php */
