e63d20788895d90a49cff42345dd492eea30fd82
abemassry <abe@massindustries.org>
2012-07-30 22:15:18 -0400

    updated pages with news info

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -2,19 +2,80 @@
 
 class Pages extends CI_Controller {
 
-	public function view($page = 'home')
+	// public function view($page = 'home')
+	// {
+	// 	if ( ! file_exists('application/views/pages/'.$page.'.php'))
+	// 	{
+	// 		// Whoops, we don't have a page for that!
+	// 		show_404();
+	// 	}
+	
+	// 	$data['title'] = ucfirst($page); // Capitalize the first letter
+	
+	// 	$this->load->view('templates/header', $data);
+	// 	$this->load->view('pages/'.$page, $data);
+	// 	$this->load->view('templates/footer', $data);
+	// }
+
+	public function __construct()
 	{
-		if ( ! file_exists('application/views/pages/'.$page.'.php'))
+		parent::__construct();
+		$this->load->model('news_model');
+	}
+
+	public function index()
+	{
+		$data['news'] = $this->news_model->get_news();
+	}
+
+	public function view($slug)
+	{
+		$data['news_item'] = $this->news_model->get_news($slug);
+
+		if (empty($data['news_item']))
 		{
-			// Whoops, we don't have a page for that!
 			show_404();
 		}
-	
-		$data['title'] = ucfirst($page); // Capitalize the first letter
-	
+
+		$data['title'] = $data['news_item']['title'];
+
 		$this->load->view('templates/header', $data);
-		$this->load->view('pages/'.$page, $data);
-		$this->load->view('templates/footer', $data);
+		$this->load->view('news/view', $data);
+		$this->load->view('templates/footer');
+	}
+
+	public function index()
+	{
+		$data['news'] = $this->news_model->get_news();
+		$data['title'] = 'News archive';
+
+		$this->load->view('templates/header', $data);
+		$this->load->view('news/index', $data);
+		$this->load->view('templates/footer');
+	}
+
+	public function create()
+	{
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		
+		$data['title'] = 'Create a news item';
+		
+		$this->form_validation->set_rules('title', 'Title', 'required');
+		$this->form_validation->set_rules('text', 'text', 'required');
+		
+		if ($this->form_validation->run() === FALSE)
+		{
+			$this->load->view('templates/header', $data);	
+			$this->load->view('news/create');
+			$this->load->view('templates/footer');
+			
+		}
+		else
+		{
+			$this->news_model->set_news();
+			$this->load->view('news/success');
+		}
 	}
 }
 ?>
