36d00da3a7ad42c4cb5b4d5f53613ccb333382ec
abemassry <abe@massindustries.org>
2012-07-30 21:48:12 -0400

    news create page

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,10 +41,12 @@
 // $route['default_controller'] = 'pages/view';
 // $route['(:any)'] = 'pages/view/$1';
 
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
 $route['default_controller'] = 'pages/view';
+
 
 // $route['default_controller'] = "Helloworld";
 // $route['404_override'] = '';
